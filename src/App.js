632e0e500e0dc10c3e651c064c9861fee6ede5a8
Cristof Morissette <cristofmorissette@Cristofs-MacBook-Air.local>
2019-07-28 14:51:37 -0600

    working on tic tac toe

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import React, { Component } from 'react';
 import './css/App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      board: Array(9).fill(null),
+      player: 'X'
+    }
+  }
+
+  handleClick = (index) => {
+    let newBoard = this.state.board;
+    if (this.state.board[index] === null){
+      newBoard[index] = this.state.player;
+    
+    this.setState({
+      board: newBoard,
+      player: this.state.player === 'X' ? 'O' : 'X'
+    })
+  }
+  }
+
   render(){
+    const Box = this.state.board.map((box, index) => <div className='box' key={index} onClick={() => this.handleClick(index)}>{box}</div>)
     return (
       <div className='container'>
         <h1>Tic Tac Toe App</h1>
         <div className='board'>
-          <div className='box'>X</div>
-          <div className='box'>X</div>
-          <div className='box'>X</div>
-          <div className='box'>X</div>
-          <div className='box'>X</div>
-          <div className='box'>X</div>
-          <div className='box'>X</div>
-          <div className='box'>X</div>
-          <div className='box'>X</div>
+          {Box}
         </div>
       </div>
   );
