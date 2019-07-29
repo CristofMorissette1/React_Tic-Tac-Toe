c7764c09fddfafbe7f01b56d68789121d43764c2
Cristof Morissette <cristofmorissette@Cristofs-MacBook-Air.local>
2019-07-29 15:07:25 -0600

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     this.state = {
       board: Array(9).fill(null),
       player: 'X',
-      winner: false
+      winner: false,
+      player1: 0,
+      player2: 0
     }
   }
 
@@ -23,11 +25,20 @@ class App extends Component {
   }
 
   checkWinner = () => {
+    let player1 = this.state.player
+    let player2 = this.state.player
     let winLines = [['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'], ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'], ['0', '4', '8'], ['2', '4', '6']]
     winLines.forEach(line => {
       const [a, b, c] = line
-      if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c]) {
+      if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c] && player1 === 'X') {
         this.setState({
+          winner: true,
+          player1: this.state.player1 + 1,
+         
+        })
+      } else if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c] && player1 === 'O'){
+        this.setState({
+          player2: this.state.player2 + 1,
           winner: true
         })
       }
@@ -48,10 +59,13 @@ class App extends Component {
   }
 
   render(){
+    const player1 = this.state.player1;
+    const player2 = this.state.player2;
     const Box = this.state.board.map((box, index) => <div className='box' key={index} onClick={() => this.handleClick(index)}>{box}</div>)
     return (
       <div className='container'>
         <button value='reset' onClick={this.gameReset}>Reset</button>
+        <h2>Player 1 Score : {player1}     Player 2 Score : {player2}</h2>
         <h1>Tic Tac Toe App</h1>
         <div className='board'>
           {Box}
